b07e44d150ab371d615d4e685cb74a5cf1dc9317
Daniel Stefan Dietiker <stefan@dfinity.org>
2021-09-09 00:27:16 +0200

    calculate tile index and rel. pos in frontend

--- src/canvas_assets/src/Submit.tsx
+++ src/canvas_assets/src/Submit.tsx
@@ -14,6 +14,9 @@ import { useContext } from "react";
 import toast from "react-hot-toast";
 import styled from "styled-components";
 import { AppContext } from "./App";
+import {
+  Position,
+} from "../../declarations/canvas_backend/canvas_backend.did";
 
 const Box = styled.div<{ background: string }>`
   height: 32px;
@@ -21,6 +24,9 @@ const Box = styled.div<{ background: string }>`
   border: 2px solid white;
   background: ${(props) => props.background};
 `;
+
+const TILE_SIZE = 64;
+const ROW_LENGTH = 16;
 
 function Submit() {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -32,9 +38,15 @@ function Submit() {
 
     setIsSubmitting(true);
     toast("Submitting your pixel");
-    // TODO: calculate the tile and relative position from the coordinates
+    let get_tile_idx = (x, y) => {
+      return Math.floor(x / TILE_SIZE) + Math.floor(y / TILE_SIZE) * ROW_LENGTH;
+    };
+    let tileIdx = get_tile_idx(absolutePosition.x, absolutePosition.y);
+    let relX = absolutePosition.x % TILE_SIZE;
+    let relY = absolutePosition.y % TILE_SIZE;
+    let relativePosition: Position = {x: relX, y: relY};
     actor
-      .update_pixel(0, absolutePosition, color)
+      .update_pixel(tileIdx, relativePosition, color)
       .then(() => {
         toast.success("Submitted! You can play again in 30 seconds");
       })
