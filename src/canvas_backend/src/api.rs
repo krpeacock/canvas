68060b51f0bc4a2ecd26fff687c3aa1d644e9a3c
Luka Skugor <luka.skugor@dfinity.org>
2021-09-08 17:48:47 +0200

    Integrate edit authorization into update call

--- src/canvas_backend/src/api.rs
+++ src/canvas_backend/src/api.rs
@@ -6,7 +6,7 @@ use ic_cdk_macros::*;
 
 use crate::{
     http_request::{HeaderField, HttpRequest, HttpResponse},
-    state::CanvasState,
+    state::{CanvasState, EditsState},
 };
 
 /// All images have square shape and the side length must be a power of 2.
@@ -35,13 +35,13 @@ pub struct Color {
 pub fn http_request(request: HttpRequest) -> HttpResponse {
     // /tile.<tile_idx>.png
     let resp = if request.url.starts_with("/tile.") {
-        let state = storage::get::<CanvasState>();
+        let canvas = storage::get::<CanvasState>();
         let rem = &request.url[6..];
         match rem.find('.') {
             None => None,
             Some(idx) => {
                 let tile_idx = (rem[..idx].parse::<u32>()).expect("parse error");
-                let tile_png = state
+                let tile_png = canvas
                     .tile_images
                     .get(tile_idx as usize)
                     .expect("invalid tile")
@@ -54,8 +54,8 @@ pub fn http_request(request: HttpRequest) -> HttpResponse {
             }
         }
     } else if request.url.starts_with("/overview.png") {
-        let state = storage::get::<CanvasState>();
-        let overview_png = state.overview_image.clone();
+        let canvas = storage::get::<CanvasState>();
+        let overview_png = canvas.overview_image.clone();
         Some(HttpResponse {
             status_code: 200,
             headers: png_header_fields(&overview_png),
@@ -74,13 +74,17 @@ pub fn http_request(request: HttpRequest) -> HttpResponse {
 
 #[update]
 fn update_pixel(tile_idx: u32, pos: Position, color: Color) {
-    let state = storage::get_mut::<CanvasState>();
-    state.update_pixel(tile_idx, pos, color);
+    let canvas = storage::get_mut::<CanvasState>();
+    let edits = storage::get_mut::<EditsState>();
+    if let Ok(_) = edits.register_edit(ic_cdk::caller(), ic_cdk::api::time()) {
+        canvas.update_pixel(tile_idx, pos, color);
+    }
 }
 
 #[init]
 fn init() {
-    let _state = storage::get_mut::<CanvasState>();
+    let _canvas = storage::get_mut::<CanvasState>();
+    let _edits = storage::get_mut::<EditsState>();
 }
 
 fn png_header_fields(body: &[u8]) -> Vec<HeaderField> {
