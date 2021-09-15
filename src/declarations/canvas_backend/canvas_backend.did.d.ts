3bc3743852807ced0aa4f02a230795685f09f45f
Kyle Peacock <kylpeacock@gmail.com>
2021-09-15 11:54:16 -0700

    edits backup

--- src/declarations/canvas_backend/canvas_backend.did.d.ts
+++ src/declarations/canvas_backend/canvas_backend.did.d.ts
@@ -18,7 +18,9 @@ export interface HttpResponse {
 }
 export interface Position { 'x' : number, 'y' : number }
 export interface _SERVICE {
+  'backup_edits' : () => Promise<Array<[Principal, bigint]>>,
   'check_cooldown' : () => Promise<bigint>,
+  'cycles' : () => Promise<bigint>,
   'http_request' : (arg_0: HttpRequest) => Promise<HttpResponse>,
   'time_left' : () => Promise<bigint>,
   'update_pixel' : (arg_0: number, arg_1: Position, arg_2: Color) => Promise<
--- src/declarations/canvas_backend/canvas_backend.did.js
+++ src/declarations/canvas_backend/canvas_backend.did.js
@@ -18,7 +18,13 @@ export const idlFactory = ({ IDL }) => {
     'r' : IDL.Nat8,
   });
   return IDL.Service({
+    'backup_edits' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Principal, IDL.Nat64))],
+        ['query'],
+      ),
     'check_cooldown' : IDL.Func([], [IDL.Nat64], ['query']),
+    'cycles' : IDL.Func([], [IDL.Nat64], ['query']),
     'http_request' : IDL.Func([HttpRequest], [HttpResponse], ['query']),
     'time_left' : IDL.Func([], [IDL.Nat64], ['query']),
     'update_pixel' : IDL.Func([IDL.Nat32, Position, Color], [], []),
